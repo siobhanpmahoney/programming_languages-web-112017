1f7a93b033dba5facaf81ea2e288f3cc422c8671
Siobhan Mahoney <siobhan.p.mahoney@gmail.com>
2017-11-04 00:27:23 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,9 +1,9 @@
   def reformat_languages(languages)
     new_hash = {}
     languages.each do |style, lang_info|
-      lang_info.each do |name, type|
+      lang_info.each do |name, type_hash|
         if !new_hash.keys.include?(name)
-          new_hash[name] = {type: type[:type], style: []}
+          new_hash[name] = {type: type_hash[:type], style: []}
         end
         new_hash[name][:style] << style
       end
